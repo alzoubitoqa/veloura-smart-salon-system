d082b5cbaf38c499d88f0363b842a159407c430c
alzoubitoqa <alzoubitoqa3@gmail.com>
2026-04-25 19:08:55 +0300

    switch database connection to postgres

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,14 +1,14 @@
-const mysql = require("mysql2");
+const { Pool } = require("pg");
 
-const db = mysql.createPool({
+const pool = new Pool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
-module.exports = db.promise();
+module.exports = pool;
